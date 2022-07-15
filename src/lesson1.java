4272bef68a717e00440365df19e1e92c0358309a
михаил <miharizhkov@gmail.com>
2022-07-15 11:14:04 +0500

    123

---
+++ src/lesson1.java
@@ -0,0 +1,13 @@
+public class lesson1 {
+    public static void main(String[] args) {
+        //Примитивы
+        boolean ansver = true;
+        System.out.println("нравится ли вам кофе?");
+        System.out.println(ansver);
+        float D = 3.12345678910f;
+        System.out.println(D);
+        System.out.println(D);
+
+    }
+    }
+
