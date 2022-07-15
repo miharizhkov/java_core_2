8e3dafd5c046b99a6bdb64c811678bd8a7b83187
михаил <miharizhkov@gmail.com>
2022-07-15 23:09:16 +0500

    дз 1

--- src/Homework/homework1.java
+++ src/Homework/homework1.java
@@ -10,12 +10,14 @@ public class homework1 {
         String A = (hi.trim()+world.toLowerCase());
         System.out.println(A+newLine+A+newLine+A);
         // Задача 2
-        double Rost = 1.85;
-        double Ves = 85.3;
-        double Imt = Ves / (Rost * Rost);
-        System.out.println(Imt);
+        double rost = 1.85;
+        double ves = 85.3;
+        double imt = ves / (rost * rost);
+        System.out.println(imt);
         // Задача 3
-
-
+        char[] a = {'a','b', 'c','d', 'e'};
+        System.out.println(a);
+        a[3] = 'h';
+        System.out.println(a);
     }
 }
